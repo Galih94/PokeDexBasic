df5b19f564c42c487f712323d7ca35ec35491269
galih <samudragalih94@gmail.com>
2025-07-30 10:05:46 +0700

    feat: update test register

--- PokeDexBasic/PokeDexBasicTests/ViewModel/LoginViewModelTests.swift
+++ PokeDexBasic/PokeDexBasicTests/ViewModel/LoginViewModelTests.swift
@@ -9,5 +9,21 @@ import XCTest
 @testable import PokeDexBasic
 
 final class LoginViewModelTests: XCTestCase {
+    func testSaveLoadDeleteCalled() {
+        let (sut, loader) = makeSUT()
+        sut.register("test name")
+        
+        XCTAssertEqual(loader.requests, [.save(User(name: "test name"))])
+    }
     
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (LoginViewModel, LocalUserDataSpy) {
+        let loader = LocalUserDataSpy()
+        let sut = LoginViewModel(loader: loader)
+        trackForMemoryLeak(sut, file: file, line: line)
+        trackForMemoryLeak(loader, file: file, line: line)
+        
+        return (sut, loader)
+    }
 }
